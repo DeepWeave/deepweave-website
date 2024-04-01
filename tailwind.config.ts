959a9c661cc150e06fba60bfa4d46a74989796f3
Lauren Dimberg <ldimberg@canallc.com>
2024-03-31 20:20:26 -0500

    Setting up routing patterns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
+/** @format */
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Arial", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
